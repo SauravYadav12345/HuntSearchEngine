51723ec219ef8b7ceaad1436d6990486d9e7f906
Saurav Yadav <yosaurav24@gmail.com>
2024-02-24 10:10:09 +0530

    create search page for web and image

---
+++ src/app/search/image/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ImageSearchPage = () => {
+  return (
+    <div className='text-red-500' >ImageSearchPage</div>
+  )
+}
+
+export default ImageSearchPage;
---
+++ src/app/search/web/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const WebSearchPage = () => {
+  return (
+    <div>WebSearchPage</div>
+  )
+}
+
+export default WebSearchPage;
