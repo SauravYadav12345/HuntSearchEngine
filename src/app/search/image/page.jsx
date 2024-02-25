7e0cf3502bb45b63c0bf959d22f8316b43156cec
Saurav Yadav <yosaurav24@gmail.com>
2024-02-25 21:47:09 +0530

    loading for page and image

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 
 export const ImageSearchPage = async ({searchParams}) => {
   const startIndex = searchParams.start || '1';
+  await new Promise((resolve) => setTimeout(resolve, 1000) );
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex} `
   );
   if(!response.ok) throw new Error('something went wrong');
