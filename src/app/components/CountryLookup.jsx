7ee38b063f51d87c948ed12082dc18eab37b3b86
Saurav Yadav <yosaurav24@gmail.com>
2024-07-09 20:47:26 +0530

    new changes

--- src/app/components/CountryLookup.jsx
+++ src/app/components/CountryLookup.jsx
@@ -1,21 +1,21 @@
 "use client ";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export const CountryLookup = () => {
-    const [country, setCountry] = useState('Delhi');
+  const [country, setCountry] = useState("Delhi");
 
-    useEffect(()=>{
-        const getCountry = async ()=>{
-            const response = await fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
-               .then((res)=> res.json())
-               .then((data)=> data.country);
-            if(!response) return;
-            setCountry(response);   
-        };
-        getCountry();
-    }, []);
-    
-  return (
-    <div>{country}</div>
-  );
-}
+  useEffect(() => {
+    const getCountry = async () => {
+      const response = await fetch(
+        `https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data.country);
+      if (!response) return;
+      setCountry(response);
+    };
+    getCountry();
+  }, []);
+
+  return <div>{country}</div>;
+};
